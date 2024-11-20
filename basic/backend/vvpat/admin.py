d3e8d35c3a5e4cdee2f9448b2e8ebe3f48fa02c1
Diamondman51 <zshavkatov51@gmail.com>
2024-11-21 03:15:05 +0500

    models added

--- basic/backend/vvpat/admin.py
+++ basic/backend/vvpat/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from vvpat.models import Director, Position, President, Voter
+
 # Register your models here.
+
+admin.site.register(Voter)
+admin.site.register(Director)
+admin.site.register(President)
+admin.site.register(Position)
--- basic/backend/vvpat/models.py
+++ basic/backend/vvpat/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Voter(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    position = models.ForeignKey('Position', on_delete=models.CASCADE)
+    image = models.ImageField(blank=False, null=True)
+    president_id = models.ForeignKey('President', on_delete=models.CASCADE)
+    directors_id = models.OneToOneField('Director', null=True, blank=True, on_delete=models.CASCADE)
+
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    image = models.ImageField(blank=False, null=True)
+
+
+class President(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    image = models.ImageField(blank=False, null=True)
+
+
+class Position(models.Model):
+    position = models.CharField(max_length=255)
