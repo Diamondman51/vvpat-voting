457efe448f5d25637fad5eca893c9e604c5d0ab3
DIAMONDMAN <Diamondman51>
2025-02-20 01:50:37 +0500

    hosts

--- basic/backend/base/settings.py
+++ basic/backend/base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0wr$=^3ai2^c8xenb8f+g=168k$+=r+8*$3p1vi*8fgly$s=kj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
